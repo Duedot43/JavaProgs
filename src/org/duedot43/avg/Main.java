e2f900891858873724371dfbfdd3b3635de8eac5
Duedot43 <gendillj@gmail.com>
2022-10-28 09:20:14 -0600

    I did it

---
+++ src/org/duedot43/avg/Main.java
@@ -0,0 +1,15 @@
+package org.duedot43.avg;
+
+public class Main {
+    public static void main(String[] args) {
+        int grade1 = 76;
+        int grade2 = 17;
+        int grade3 = 64;
+
+        int sum = grade1 + grade2 + grade3;
+
+        double avg = sum / 3.0;
+
+        System.out.println("The average of your grades is " + avg);
+    }
+}
---
+++ src/org/duedot43/dog_age/Main.java
@@ -0,0 +1,13 @@
+package org.duedot43.dog_age;
+
+public class Main {
+    public static void main(String[] args) {
+        int yourAge = 30;
+        int dogAge = 3;
+        int realDogAge = dogAge * 7;
+
+        System.out.println("Your age: " + yourAge);
+        System.out.println("Your dogs age: " + dogAge);
+        System.out.println("Real dog age: " + realDogAge);
+    }
+}
